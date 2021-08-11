f4918fa3023582c1e4822fff458712b39fc001d6
matankribus <matanwx135@gmail.com>
2021-08-12 00:07:15 +0300

    Continued development.

--- app/src/main/java/com/example/ppn/SubActivity.java
+++ app/src/main/java/com/example/ppn/SubActivity.java
@@ -1,30 +1,56 @@
 package com.example.ppn;
 
+
+/**
+ * designed as a subclass for {@link ActivityTask} to help the user separate tasks into smaller actions
+ *
+ */
 public class SubActivity {
+    /**
+     * the discription of the action to be done.
+     */
     private String content;
-    private int subActivityID ;
+    /**
+     * the holding {@link ActivityTask#activityTaskID}
+     */
     private int activityTaskID;
 
+    /**
+     *
+     * @param content description of the SubActivity
+     */
     public void setContent(String content) {
         this.content = content;
     }
 
+    /**
+     *
+     * @return the description of the SubActivity
+     */
     public String getContent() {
         return content;
     }
 
+    /**
+     * firestore requires an empty constructor to rebuild classes.
+     */
     public SubActivity() {
     }
 
+    /**
+     *
+     * @param content the description of the SubActivity
+     * @param activityTaskID the {@link ActivityTask#activityTaskID} of the {@link SubActivity}
+     */
     public SubActivity(String content, int activityTaskID) {
         this.content = content;
         this.activityTaskID = activityTaskID;
     }
 
-    public int getSubActivityID() {
-        return subActivityID;
-    }
-
+    /**
+     *
+     * @return the activityTaskID this SubActivity is related to.
+     */
     public int getActivityTaskID() {
         return activityTaskID;
     }
--- app/src/main/java/com/example/ppn/TimePack.java
+++ app/src/main/java/com/example/ppn/TimePack.java
@@ -11,6 +11,8 @@ import java.util.HashMap;
 
 /**
  * A helper class to centralize time operations.
+ * <p><b>note:</b> it is easyer to work with the already supported fields in firestore, hench the heavy use of Strings</p>
+ *
  */
 public class TimePack {
 
@@ -20,7 +22,7 @@ public class TimePack {
      */
     private String startingTime;
     /**
-     *      * a String form of localedatetime, the ending time of the timepack.
+     *       a String form of localedatetime, the ending time of the timepack.
      */
     private String endingTime;
     /**
@@ -46,17 +48,25 @@ public class TimePack {
     /**
      * natty results from parsing the content of the related activityTask
      */
-    private String strigifiedNattyResults;
-
-    private ArrayList<Integer> relaventDatesNumbered = new ArrayList<>();// FIXME: 25/07/2021 is not inserted into firebase correctly. Possible issue with flow of setter/getter.
-
-
-
-
-
+    private String stringifiedNattyResults;
+    /**
+     * number correlating to this months days, if the number exist in the array the {@link ActivityTask} is relevant to that date.
+     */
+    private ArrayList<Integer> relaventDatesNumbered = new ArrayList<>();
+    /**
+     * firestore requires empty constructor to create class
+     */
     public TimePack() {
     }
-
+    /**
+     * <p>sets the starting time and ending time of the {@link #startingTime} and {@link #endingTime} according to the first and secound elements of timeRange paramater, any other elements are not used</p>
+     * <p>uses the repetition parmeter to calculate {@link #relaventDatesNumbered} form the starting time recived.</p>
+     *<p>{@link #stringifiedNattyResults} is set by default to now, incase natty will not be able to parse {@link ActivityTask#content} for a date</p>
+     * @param timeRange expected to hold 2 values, the first is the starting time of the {@link TimePack} the second is the ending time
+     * @param monthNumber the month number where this {@link TimePack} is relevant to. 1-12
+     * @param repetition {@link Repetition} as to how the {@link TimePack} is to be repeated.
+     * @param strigifiedRelaventDates stringified {@link LocalDateTime}s in the format {@link #getFormatter()} gives.
+     */
     public TimePack(ArrayList<LocalDateTime> timeRange, int monthNumber,Repetition repetition,ArrayList<String> strigifiedRelaventDates) {
 
         try {
@@ -71,7 +81,7 @@ public class TimePack {
         setMonthNumber(monthNumber);
         setRepetition(repetition);
         setStrigifiedRelaventDates(strigifiedRelaventDates);
-        this.strigifiedNattyResults = LocalDateTime.now().format(getFormatter());
+        this.stringifiedNattyResults = LocalDateTime.now().format(getFormatter());
 
         notificationCounter = notificationCounter +1;
         for (String string:
@@ -81,6 +91,17 @@ public class TimePack {
 
     }
 
+    /**
+     *
+     * <p>calculates relevant dates according to the repetition parameter and adds the one sin the relevantDates parameter</p>
+     * <p>sets {@link #stringifiedNattyResults} to {@link LocalDateTime#now()} using {@link #getFormatter()}</p>
+     * <p></p>
+     * @param startingTime starting time of the {@link TimePack}.
+     * @param endingTime ending time of the {@link TimePack}.
+     * @param monthNumber the month number relevant for this {@link TimePack}.
+     * @param repetition {@link Repetition} as to represent how this {@link TimePack} is to be repeated.
+     * @param relaventDates an array reprasenting the relavent dates to which this {@link TimePack } is relevant to.
+     */
     public TimePack(LocalDateTime startingTime,LocalDateTime endingTime, int monthNumber,Repetition repetition,ArrayList<LocalDateTime> relaventDates) {
 
         setStartingTime(startingTime.format(getFormatter()));
@@ -91,7 +112,7 @@ public class TimePack {
         this.monthNumber = monthNumber;
         setRepetition(repetition);
         updateRelaventDates(relaventDates);
-        this.strigifiedNattyResults = LocalDateTime.now().format(getFormatter());
+        this.stringifiedNattyResults = LocalDateTime.now().format(getFormatter());
 
         notificationCounter = notificationCounter +1;
         for (String string:
@@ -101,37 +122,49 @@ public class TimePack {
 
     }
 
+    /**
+     * a class ment to be used with data returned from firestore, should only be used if something gone wrong and there is a problem with {@link com.google.firebase.firestore.DocumentSnapshot#toObject(Class)}
+     * @param mappedData the mapped data that build the TimePack, keys are the member names and values are their content.
+     */
     public TimePack(HashMap<String ,Object> mappedData) {
-
-
-
         this.startingTime = (String) mappedData.get("startingTime");
         this.endingTime = (String) mappedData.get("startingTime");
         this.monthNumber = ((Long) mappedData.get("monthNumber")).intValue();
         this.strigifiedRelaventDates = (ArrayList<String>) mappedData.get("strigifiedRelaventDates");
-        this.strigifiedNattyResults = (String) mappedData.get("strigifiedNattyResults");
+        this.stringifiedNattyResults = (String) mappedData.get("strigifiedNattyResults");
         this.repetition = Repetition.valueOf((String) mappedData.get("repetition"));
         this.notificationID = ((Long) mappedData.get("notificationID")).intValue();
-
     }
 
+    /**
+     * converts {@link LocalDateTime} to a string and assings to {@link #stringifiedNattyResults} using {@link #getFormatter()}
+     * @param nattyResults
+     */
     public void updateNattyResults(LocalDateTime nattyResults) {
-        this.strigifiedNattyResults = nattyResults.format(getFormatter());
+        this.stringifiedNattyResults = nattyResults.format(getFormatter());
     }
 
-    public void setStrigifiedNattyResults(String strigifiedNattyResults) {
-        this.strigifiedNattyResults = strigifiedNattyResults;
+    /**
+     * setter for {@link #stringifiedNattyResults}
+     * @param stringifiedNattyResults
+     */
+    public void setStringifiedNattyResults(String stringifiedNattyResults) {
+        this.stringifiedNattyResults = stringifiedNattyResults;
     }
 
-    public String getStrigifiedNattyResults() {
-        return strigifiedNattyResults;
+    /**
+     * getter for {@link #stringifiedNattyResults}
+     * @return {@link #stringifiedNattyResults}
+     */
+    public String getStringifiedNattyResults() {
+        return stringifiedNattyResults;
     }
 
+    /**
+     * adds relevant dates to {@link #relaventDatesNumbered} according to {@link #repetition}
+     */
     public void reCalculateReleventDates(){
         ArrayList<LocalDateTime> newRelaventDates = new ArrayList<>();
-
-
-
         switch (this.repetition){
             case No_repeting: return;
             //region Every_24_hours
@@ -142,7 +175,6 @@ public class TimePack {
                 newRelaventDates.add(readTimeTange().get(0).plusDays(3));
                 return;
             //endregion
-
             //region every_week
             case every_week:
                 newRelaventDates.add(readTimeTange().get(0));
@@ -214,11 +246,13 @@ public class TimePack {
                 return;
             //endregion
         }
-
         updateRelaventDates(newRelaventDates);
-
     }
 
+    /**
+     *
+     * @return an {@link ArrayList} of {@link LocalDateTime} containt the starting time in position 0 and ending time in position 1
+     */
     public ArrayList<LocalDateTime> readTimeTange() {
         ArrayList<LocalDateTime> timeRange = new ArrayList<>();
         timeRange.add(LocalDateTime.parse(startingTime,getFormatter()));
@@ -227,36 +261,53 @@ public class TimePack {
         return timeRange;
     }
 
+    /**
+     * @return {@link LocalDateTime} got from the content of the {@link ActivityTask} related to this TimePack, if it is not relevent to an {@link ActivityTask} or natty could not parse a date, it will be the result of the {@link LocalDateTime#now()} at the time of creation
+     */
     public LocalDateTime readNattyResults() {
-        return LocalDateTime.parse(strigifiedNattyResults,getFormatter());
+        return LocalDateTime.parse(stringifiedNattyResults,getFormatter());
     }
     /**
-     * used to parse and covert strings into LocalDateTime
+     * used to parse and covert strings into {@link LocalDateTime}, to remain consistent with setting and getting.
      */
     public static DateTimeFormatter getFormatter(){
         return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     }
 
+    /**
+     * @return {@link #startingTime}
+     */
     public String getStartingTime() {
         return startingTime;
     }
 
+    /**
+     * @return {@link #endingTime}
+     */
     public String getEndingTime() {
         return endingTime;
     }
 
     /**
      * !!DO NOT USE!!. is required for firebase to work with the class.
-     * @param notificationID
+     * @param notificationID {@link #notificationID}
      */
     public void setNotificationID(int notificationID) {
         this.notificationID = notificationID;
     }
 
+    /**
+     *
+     * @param startingTime the strigified {@link LocalDateTime} formated with {@link #getFormatter()}
+     */
     public void setStartingTime(String startingTime) {
         this.startingTime = startingTime;
     }
 
+    /**
+     *
+     * @param endingTime the stringified {@link LocalDateTime} foramted with {@link #getFormatter()}
+     */
     public void setEndingTime(String endingTime) {
         this.endingTime = endingTime;
     }
@@ -265,14 +316,25 @@ public class TimePack {
         return TAG;
     }
 
+    /**
+     * @return {@link #relaventDatesNumbered}
+     */
     public ArrayList<Integer> getRelaventDatesNumbered() {
         return relaventDatesNumbered;
     }
 
+    /**
+     *  mainly used by firestore
+     * @param relaventDatesNumbered an array of numbers corresponding to this {@link TimePack} relevant dates for this month
+     */
     public void setRelaventDatesNumbered(ArrayList<Integer> relaventDatesNumbered) {
         this.relaventDatesNumbered = relaventDatesNumbered;
     }
 
+    /**
+     * adds the relevant dates array of {@link LocalDateTime} to the
+     * @param relaventDates
+     */
     public void updateRelaventDates(ArrayList<LocalDateTime> relaventDates) {
         if (relaventDates != null) {
 
@@ -283,6 +345,7 @@ public class TimePack {
                     relaventDates) {
                 settedStrings.add(localDateTime.format(getFormatter()));
             }
+            strigifiedRelaventDates.addAll(settedStrings);
         }
     }
     public ArrayList<LocalDateTime> readRelaventDates() {
