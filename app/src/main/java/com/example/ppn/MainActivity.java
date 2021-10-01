4ce3da07d6c838e3b3a92d99a61b27dd9755e066
lior3300 <jacobtherobber@gmail.com>
2021-10-01 13:55:01 +0300

    fixed a date bug anf line 318 in AddReminder

--- app/src/main/java/com/example/ppn/MainActivity.java
+++ app/src/main/java/com/example/ppn/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.ppn;
 
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,20 +8,14 @@ import androidx.viewpager2.widget.ViewPager2;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 /**
  *
